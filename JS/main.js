d2e1017b4572747552b33036d4c8bb15c486665d
“Pablo <Pablo_martin_caballero21@hotmail.comgit config user.emailgir user.namegit user.namegit config --global user.name Pablo>
2023-07-28 18:41:56 -0300

    Nuevos Cambios

--- JS/main.js
+++ JS/main.js
@@ -1,87 +1,76 @@
 
-//Creación de la clase ProductManager
-class ProductManager {
-    constructor(products){
-        this.products = products
-    }
-    
-    addProducts(Product){
-
-        //Utiliozo el metodo entries para transformar al objeto que va a ser ingresado en un array
-        
-            let ObjectEntries = Object.entries(Product)
-            console.log(ObjectEntries);
-        
-        //Filtro la propiedad code 
-        
-            let filterCode = ObjectEntries.filter((code)=>
-            code.includes("code")
-            )
-            console.log(filterCode)
-
-        //Itero sobre el array de objetos existentes en PM
-        
-            this.products.forEach((products)=>{
-        
-        //Convierto al objeto iterado en un array
-        
-            let productsArray = Object.entries(products)
-            console.log(productsArray)
-               
-        //Filtro la propiedad code de cada uno de los objetos dentro
-
-            let productsCode = productsArray.filter((code)=>
-            code.includes("code")
-            );
-        
-             const compareArray = (filterCode,productsCode) => {
-           
-                let result = filterCode.every((arr,i)=> arr === productsCode[i])
-                console.log(result)
-                return result
-                        
-                }
-        
-            let compCodes = compareArray(filterCode,productsCode)
-            console.log(compCodes)
-        
-        //Necesito traer todas las priedades code de ProductManager y compararlas con las del objeto entrante (FilterCode) este metodo se puede utilizar para getProducts
-                
-            if (compCodes == false) {
-                this.products.push(Product)
-            } else {
-                console.log("El producto ya existe")
-            }
-        
-        })
-        
+    class ProductManager {
+        constructor(){
+            this.products = [];
         }
+
+        getProduct(){
+            return this.product;
+        }
+
+        getProductById (id){
+            let product = this.product.find(product => product.id == id)
+
+            if(product){
+                return product
+            } else {
+                console.log("No se encontro el producto")
+            }
+        }
+
+        addProduct(product) {
+            if(this.products.find( prod => prod.code == product.code)){
+                return "Existing product"
+            } 
+            
+            if (product.code != "" || product.stock >= 0) {
+                this.products.push(product)
+            } else {
+                return "Empy product"
+            }
+
+            
+        }
+
     }
-    
 
-//Creación de la clase Product
-class Product {
-    constructor(title,description,price,thumbnail,code,stock){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
+
+    class product {
+        constructor(title, description, price, thumbnail, code, stock){
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.thumbnail = thumbnail;
+            this.code = code;
+            this.stock = stock;
+            this.id = product.increaseID()
+        }
+
+         static increaseID(){
+            if(this.idIncrement){
+                this.idIncrement++
+            } else {
+                this.idIncrement = 1 
+            }
+            return this.idIncrement
+         }
     }
-}
 
 
+const PRODUCT1 = new product ("Bass", "Fender Bass", 80000, "", 2244, 20);
 
-const PRODUCT1 = new Product("Bass","Instrument",70000,"https://images.ctfassets.net/m8onsx4mm13s/5hbt5as9BU0fGz2lh7PUm5/f5189313b5fdaf068e57b601a6f42313/__static.gibson.com_product-images_Kramer_KRA65W106_Satin_Black_KDB1OSBBH1_front.jpg" ,1234, 5)
+const PRODUCT2 = new product ("Guitar", "Gibson Guitar", 90000, "", 2245, 20);
 
-const PRODUCT2 = new Product("Guitar","Instrument",80000,"https://images.ctfassets.net/m8onsx4mm13s/5hbt5as9BU0fGz2lh7PUm5/f5189313b5fdaf068e57b601a6f42313/__static.gibson.com_product-images_Kramer_KRA65W106_Satin_Black_KDB1OSBBH1_front.jpg" ,1235, 5)
+console.log(PRODUCT1);
+console.log(PRODUCT2);
+
+const productManager = new ProductManager();
+
+productManager.addProduct(PRODUCT1);
+productManager.addProduct(PRODUCT2);
+
+console.log(productManager);
 
 
-const PRODUCT_MANAGER = new ProductManager([]);
-    console.log(PRODUCT_MANAGER);
-
-PRODUCT_MANAGER.addProducts(PRODUCT1)
-console.log(PRODUCT_MANAGER)
 
 
